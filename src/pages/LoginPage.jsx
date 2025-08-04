bd7d2bb9eb85c5d993cc471ec0fd67fca01d2497
Glender Nieto <glennder2020@gmail.com>
2025-08-04 16:50:20 -0400

    construyendo el login

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+export const LoginPage = () => {
+  return (
+    <main className="flex h-screen w-full"> 
+        {/* Lado izquierdo - banner azul */}
+        <section className="bg-[#]">
+            izquierda
+        </section>
+        {/* Lado derecha - formulario de inicio de seccion*/}
+        <section>
+
+        </section>
+    </main>
+  )
+}
+
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"; // Importamos BrowserRouter
 import { MainLayout } from "../layouts/MainLayout";
 import { HomePage } from "../pages/HomePage"; // Asegúrate de que esta ruta sea correcta
+import { LoginPage } from "../pages/LoginPage";
 
 export function MyRouter() {
   return (
     <BrowserRouter> {/* Envolvemos las rutas con BrowserRouter para manejar el enrutamiento */}
       <Routes>
+        <Route path="/login" element={<LoginPage />} /> {/* Ruta para la página de login */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
         </Route>
