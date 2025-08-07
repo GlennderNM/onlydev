076ded0ac0c511b805f13d6c1a14978c0c9ef9ea
Glender Nieto <glennder2020@gmail.com>
2025-08-07 15:34:04 -0400

    useSubcription

--- src/store/AuthStore.jsx
+++ src/store/AuthStore.jsx
@@ -1,14 +1,34 @@
 import { create } from "zustand";
 import { supabase } from "../supabase/supabase.config";
 
-export const useAuthStore = create((set) => ({
-    credenciales: null,
-    setCredenciales: (p) => set({ credenciales:p }),
-    crearUserYLogin: async (p) => {
-        const {data} = await supabase.auth.signUp({
+export const useAuthStore = create((set) => ({ // Store para manejar la autenticación
+    credenciales: null, // Estado para almacenar las credenciales del usuario
+    setCredenciales: (p) => set({ credenciales:p }), // Función para actualizar las credenciales
+    crearUserYLogin: async (p) => { // Función para crear un usuario y iniciar sesión
+        const {data} = await supabase.auth.signUp({ // Llama a la API de Supabase para crear un usuario
             email: p.email,
             password: p.password 
         })
         return data;
     }
 }))
+
+export const useSubcription = create((set) => {// Store para manejar la suscripción
+    const store = {
+        user: null, // Estado para almacenar el usuario autenticado
+        setUser: (user) => set({ user }), // Función para actualizar el usuario
+    };
+    supabase.auth.getSession().then(({ data:{session} }) => { // Obtiene la sesión actual del usuario
+        if (session?.user) {
+            set({ user: session.user  }); // Si hay una sesión, actualiza el usuario en el store
+        }
+    });
+    supabase.auth.onAuthStateChange((_event, session)=>{
+        if(session?.user){
+            set({user: session.user})
+        }else{
+            set({user: null})
+        }
+    })
+    return store;
+})
