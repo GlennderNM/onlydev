525e43522bdb30d16962ef29b4d539780b511e60
Glender Nieto <glennder2020@gmail.com>
2025-09-06 22:03:38 -0400

    Subiendo archivos

--- src/store/postStore.jsx
+++ src/store/postStore.jsx
@@ -1,14 +1,63 @@
 import { create } from "zustand";
+import { supabase } from "../supabase/supabase.config";
+
+const tabla = "publicaciones";
+
+const insertarPost = async (p, file) => {
+  const { data, error } = await supabase
+    .from(tabla)
+    .insert(p)
+    .select()
+    .maybeSingle();
+  if (error) {
+    throw new Error(error.message);
+  }
+  if (file) {
+    const nuevo_id = data?.id;
+    const urlImage = await subirArchivo(nuevo_id, file);
+    const pUrl = {
+      url: urlImage,
+      id: nuevo_id,
+    };
+    await EditarPublicacion(pUrl);
+  }
+};
+
+const EditarPublicacion = async (p) => {
+  const { error } = await supabase.from(tabla).update(p).eq("id", p.id);
+  if (error) {
+    throw new Error(error.message);
+  }
+};
+
+const subirArchivo = async (id, file) => {
+  const ruta = "publicaciones/" + id;
+  const { data, error } = await supabase.storage
+    .from("archivos")
+    .upload(ruta, file, {
+      cacheControl: "0",
+      upsert: true,
+    });
+  if (error) {
+    throw new Error(error.message);
+  }
+  if (data) {
+    const { data: urlimage } = await supabase.storage
+      .from("archivos")
+      .getPublicUrl(ruta);
+    return urlimage;
+  }
+};
 
 export const usePostStore = create((set) => ({
-    file: null,
-    setFile: (p) => set({file: p}),
-    stateImage: false,
-    setStateImage: () => {
-        set((state) => ({stateImage: !state.stateImage}))
-    },
-    stateForm: false,
-    setStateForm: () => {
-        set((state) => ({stateForm: !state.stateForm}))
-    }
-}))
+  file: null,
+  setFile: (p) => set({ file: p }),
+  stateImage: false,
+  setStateImage: () => {
+    set((state) => ({ stateImage: !state.stateImage }));
+  },
+  stateForm: false,
+  setStateForm: () => {
+    set((state) => ({ stateForm: !state.stateForm }));
+  },
+}));
