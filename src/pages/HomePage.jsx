950e55abb94e81cce5b71946aff5edc77403a8b1
Glender Nieto <glennder2020@gmail.com>
2025-09-10 14:29:54 -0400

    Agregando referencia de scroll

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,20 +6,49 @@ import { FormsPost } from "../components/Forms/FormsPost";
 import { usePostStore } from "../store/postStore";
 import { Toaster } from "sonner";
 import { useMostrarPostQuery } from "../stack/PostStack";
+import { useEffect, useRef } from "react";
 
 export const HomePage = () => {
   const { stateForm, setStateForm } = usePostStore();
-  const {data} = useMostrarPostQuery()
+  const {
+    data: dataPost,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading: isLoadingPost,
+  } = useMostrarPostQuery();
+  const scrollRef = useRef(null);
+  useEffect(() => {
+    const el = scrollRef.current;
+    const handleScroll = () => {
+      if (
+        el.scrollTop + el.clientHeight >= el.scrollHeight - 200 &&
+        hasNextPage &&
+        !isFetchingNextPage
+      ) {
+        fetchNextPage();
+      }
+    };
+    if (el) {
+      el.addEventListener("scroll", handleScroll);
+      return () => el.removeEventListener("scroll", handleScroll);
+    }
+  }, [fetchNextPage, hasNextPage, isFetchingNextPage]);
+
   return (
     <main className="flex min-h-screen bg-white dark:bg-bg-dark max-w-[1200px] mx-auto">
-      <Toaster position="top-left"/>
+      <Toaster position="top-left" />
       {stateForm && <FormsPost />}
       <section className="flex flex-col w-full h-screen">
         <article className="flex flex-col h-screen overflow-hidden border border-gray-200 border-t-0 border-b-0 dark:border-gray-600">
           <HeaderSticky />
-          <div className="overflow-y-auto">
+          <div ref={scrollRef} className="overflow-y-auto">
             <InputPublicar />
-            <PublicacionCard />
+            {dataPost?.pages?.map((page, pageIndex) =>
+              page?.map((item, index) => (
+                <PublicacionCard key={`${pageIndex} - ${index}`} item={item} />
+              ))
+            )}
           </div>
           Principal
         </article>
